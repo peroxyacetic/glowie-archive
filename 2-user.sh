aa36216824e9009f970253e43cd6647d0de281b0
snow <94102452+peroxyacetic@users.noreply.github.com>
2021-12-11 13:49:46 +0100

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -44,16 +44,6 @@ done
 export PATH=$PATH:~/.local/bin >/dev/null 2>&1
 rsync -rtv /home/snow/glowie/dotfiles/ /home/snow/ >/dev/null 2>&1
 
-# Tap to click
-[ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
-        Identifier "libinput touchpad catchall"
-        MatchIsTouchpad "on"
-        MatchDevicePath "/dev/input/event*"
-        Driver "libinput"
-	# Enable left mouse button by tapping
-	Option "Tapping" "on"
-EndSection' > /etc/X11/xorg.conf.d/40-libinput.conf >/dev/null 2>&1
-
 # Fix fluidsynth/pulseaudio issue.
 grep -q "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" /etc/conf.d/fluidsynth ||
 	echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >> /etc/conf.d/fluidsynth >/dev/null 2>&1
