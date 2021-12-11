607cf357a7b135369e301e8eddecdf7ebe283c8a
snow <94102452+peroxyacetic@users.noreply.github.com>
2021-12-11 14:34:12 +0100

    11

--- 2-user.sh
+++ 2-user.sh
@@ -3,13 +3,14 @@
 echo -e "\nINSTALLING AUR SOFTWARE\n"
 # You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
 
-echo "CLONING: YAY"
+echo "INSTALLING: YAY"
 mkdir /home/snow/source
 cd /home/snow/source >/dev/null 2>&1
 git clone "https://aur.archlinux.org/yay.git" >/dev/null 2>&1
 cd yay >/dev/null 2>&1
 makepkg -si --noconfirm >/dev/null 2>&1
 cd /home/snow/source >/dev/null 2>&1
+echo "INSTALLING: Ani-CLI"
 git clone "https://github.com/pystardust/ani-cli.git" >/dev/null 2>&1
 cd ani-cli >/dev/null 2>&1
 sudo make >/dev/null 2>&1
@@ -29,22 +30,16 @@ PKGS=(
 'clearine'
 'betterlockscreen'
 'spotify'
+'jellyfin-media-player'
 )
 
 for PKG in "${PKGS[@]}"; do
-    echo "Installing $PKG"
+    echo "INSTALLING: $PKG"
     yay -S --noconfirm $PKG >/dev/null 2>&1
 done
 
 export PATH=$PATH:~/.local/bin >/dev/null 2>&1
 rsync -rtv /home/snow/glowie/dotfiles/ /home/snow/ >/dev/null 2>&1
-
-# Fix fluidsynth/pulseaudio issue.
-grep -q "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" /etc/conf.d/fluidsynth ||
-	echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >> /etc/conf.d/fluidsynth >/dev/null 2>&1
-
-# Start/restart PulseAudio.
-pkill -15 -x 'pulseaudio'; sudo -u "$name" pulseaudio --start >/dev/null 2>&1
 
 echo -e "\nDone!\n"
 exit
