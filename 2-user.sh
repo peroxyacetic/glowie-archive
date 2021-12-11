af6ae53ee700f4441c19342404b960fb16d62643
snow <94102452+peroxyacetic@users.noreply.github.com>
2021-12-11 14:51:02 +0100

    12

--- 2-user.sh
+++ 2-user.sh
@@ -16,8 +16,8 @@ cd ani-cli >/dev/null 2>&1
 sudo make >/dev/null 2>&1
 cd ~ >/dev/null 2>&1
 touch "$HOME/.cache/zshhistory" >/dev/null 2>&1
-git clone "https://github.com/ChrisTitusTech/zsh" >/dev/null 2>&1
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k >/dev/null 2>&1
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k >/dev/null 2>&1
 ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc >/dev/null 2>&1
 git clone "https://github.com/peroxyacetic/glowie-icons" >/dev/null 2>&1
 rsync -rtv /home/snow/glowie-icons/ /home/snow/ >/dev/null 2>&1
